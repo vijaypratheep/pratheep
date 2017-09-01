97e81652402bb5449f52eb0f5c758e7948debeb0
vijaypratheep <vijaypratheep16@gmail.com>
2017-09-01 14:22:49 +0530

    Create oez.java

---
+++ oez.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class oez
+{
+public static void main(String []args)
+{
+Scanner sc=new Scanner(System.in);
+int a= sc.nextInt();
+if(a<0)
+{
+System.out.print("negative");
+}
+else if(a>0)
+{
+System.out.print("positive");
+}
+else
+{
+System.out.print("zero");
+}
+}
+}
