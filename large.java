d43e590abdfaf883c2cddd9252d7de19cb60a383
vijaypratheep <vijaypratheep16@gmail.com>
2017-09-04 15:08:36 +0530

    Update large.java

--- large.java
+++ large.java
@@ -4,5 +4,12 @@ public class large
 public static void main(String []args)
 {
 Scanner sc=new Scanner(System.in);
-int q[0]=Integer.parseInt(sc.nextLine());
-
+String a[]=new String[3];
+for(int i=0;i<3;i++)
+  {
+  a[i]=sc.nextLine();
+  }
+Arrays.sort(a);
+ System.out.print(a[2]);
+  }
+  }
